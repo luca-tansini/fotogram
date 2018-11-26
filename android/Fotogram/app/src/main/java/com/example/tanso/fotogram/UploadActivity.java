b4f9f64a01656d3e7c46ac9c6ef09b60718bc7c8
Luca Tansini <luca.tansini@studenti.unimi.it>
2018-11-26 14:35:44 +0100

    Iniziata Upload activity

--- android/Fotogram/app/src/main/java/com/example/tanso/fotogram/UploadActivity.java
+++ android/Fotogram/app/src/main/java/com/example/tanso/fotogram/UploadActivity.java
@@ -1,29 +1,86 @@
 package com.example.tanso.fotogram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class UploadActivity extends AppCompatActivity {
 
     private MyNavigationItemSelectedListener myNavigationItemSelectedListener;
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
 
+        //Toolbar
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         myToolbar.setTitle("Create post - choose a picture");
         myToolbar.setTitleTextAppearance(this,R.style.upload_toolbar_text);
+        myToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.action_next:
+                        Log.d("ajeje","next");
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
 
+        //Navigation
         BottomNavigationView nav = findViewById(R.id.navigation);
         nav.setSelectedItemId(R.id.navigation_upload);
         myNavigationItemSelectedListener = new MyNavigationItemSelectedListener(this);
         nav.setOnNavigationItemSelectedListener(myNavigationItemSelectedListener);
 
+        //Upload button
+        Button upload = findViewById(R.id.buttonUpload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseFromGallery();
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (data != null && requestCode == PICK_IMAGE) {
+            Uri imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                ImageView iw = findViewById(R.id.imageViewUpload);
+                iw.setImageBitmap(bitmap);
+                Toolbar toolbar = findViewById(R.id.my_toolbar);
+                toolbar.inflateMenu(R.menu.action_bar);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void chooseFromGallery(){
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        startActivityForResult(pickIntent, PICK_IMAGE);
     }
 
     @Override
